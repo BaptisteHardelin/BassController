af468865d260f8157dc435aa4262b9116554670a
BaptisteHardelin <baptiste.hardelin@gmail.com>
2025-06-10 22:24:16 +0200

    feat: retreive index of the sound card for the bass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer;
+
 public class Main {
+
+    // Focusrite USB audio index 12
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Mixer.Info[] mixers = AudioSystem.getMixerInfo();
+        for (int i = 0; i < mixers.length; i++) {
+            System.out.println(i + ": " + mixers[i].getName());
+        }
     }
 }
