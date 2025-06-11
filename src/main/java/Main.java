93f82b721031e55fa64804f0eb97c577970ec192
BaptisteHardelin <baptiste.hardelin@gmail.com>
2025-06-11 22:52:48 +0200

    feat: get available inputs for the focusrite is 12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Mixer;
+import javax.sound.sampled.Mixer.Info;
 
 public class Main {
 
     // Focusrite USB audio index 12
-    public static void main(String[] args) {
-        Mixer.Info[] mixers = AudioSystem.getMixerInfo();
+    public static void getAvailableInput() {
+        System.out.println("List Of Available Input :");
+        Info[] mixers = AudioSystem.getMixerInfo();
         for (int i = 0; i < mixers.length; i++) {
-            System.out.println(i + ": " + mixers[i].getName());
+            System.out.println(i + ": " + mixers[i].getName() + " - " + mixers[i].getDescription());
         }
+    }
+
+    public static void main(String[] args) {
+        getAvailableInput();
     }
 }
